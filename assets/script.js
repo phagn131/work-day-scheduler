f03738c998313242cbd7a4bb63a692d147d4a36f
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-12 23:41:01 -0400

    "updated style sheets, readme, js"

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// Use Moment.js to format the following variables:
+// 1. What is today's date in the following format: Jan 1st, 1999?
+//var today = moment();
+//$("currentDay").text(today.format("MMM Do, YYYY"));
