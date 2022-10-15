30eeabf2b55f0fea350c3b97ee80d01722d37a13
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-14 22:37:41 -0400

    "add grid in html for responsive layout, local storage for events, and linked events to time"

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,30 @@
-var $ans1a = $("#currentDay");
+var $today = $("#currentDay");
 
 var currentDay = moment();
 
 document.getElementById("currentDay").innerHTML = currentDay;
-$ans1a.text(currentDay.format("dddd, MMMM Do"));
+$today.text(currentDay.format("dddd, MMMM Do"));
+
+
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
+
+$.each(timeBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
+
+
+$(".saveBtn").on("click", function (event) {
+  var calendarItem =
+    event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+});
 
 
