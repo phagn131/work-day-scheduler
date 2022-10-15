2b0a3b50b27f9b324ddbca79af00f3271bd955db
phagn131 <97428101+phagn131@users.noreply.github.com>
2022-10-15 01:10:44 -0400

    "add functions in JS with JQ, modified and appended hour blocks"

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
+//creates element for day using JQuery selector, modify the element and append it to the page
+
 var $today = $("#currentDay");
-
 var currentDay = moment();
-
 document.getElementById("currentDay").innerHTML = currentDay;
 $today.text(currentDay.format("dddd, MMMM Do"));
-
-
+//set variable for hour block using moment.JS
 var timeBlock = $(".hour");
 var now = parseInt(moment().format("H"));
 
+//sets function to modify hour block, and sets parameters for what class to call 
 $.each(timeBlock, function (i, hour) {
   var hourId = parseInt($(this).attr("id"));
   if (hourId === now) {
@@ -19,12 +19,19 @@ $.each(timeBlock, function (i, hour) {
     $(this).next().addClass("future");
   }
 });
-
-
+//saves calendar items with function using JQuery selector on button, click event and sets data
+//and modifies and appends the items to local storage
 $(".saveBtn").on("click", function (event) {
   var calendarItem =
     event.target.parentElement.previousElementSibling.children[0].value;
-  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+    localStorage.setItem(event.target.attributes[0].value, calendarItem);
 });
-
-
+//Sets function for each hour to modify and append to local storage, if one is selected
+$(document).ready(function () {
+  if (localStorage["9am"] !== null && localStorage["9am"] !== undefined) {
+    var nineAm = $("<p>" + localStorage["9am"] + "</p>");
+    $("#nineAm").append(nineAm[0].innerText);
+  } else {
+    ("");
+  }
+});
